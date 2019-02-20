fdd29d286ed62a24f6484a0f878dc0f3e6f5b10e
DaniilAnd <lisichkindan@yandex.ru>
2019-02-20 20:49:48 +0300

    Cleaner All, new 2 file ( Test )

---
+++ decor.cpp
@@ -0,0 +1,18 @@
+﻿#include <QtCore/QCoreApplication>
+#include "cstdlib"
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <clocale>
+#include <vector>
+#include <cmath>
+#include <QDebug>
+#include "union.h"
+
+class Factorial
+{
+public:
+    int Fac(int F){
+        return F * Fac(F-1);
+    }
+};
---
+++ registeralgorithm.cpp
@@ -0,0 +1,16 @@
+﻿#include <QtCore/QCoreApplication>
+#include "cstdlib"
+#include <iostream>
+#include <fstream>
+#include <istream>
+#include <clocale>
+#include <vector>
+#include <cmath>
+#include <QDebug>
+#include "union.h"
+
+using namespace std;
+
+int add(int a, int b){
+    return a +b;
+}
