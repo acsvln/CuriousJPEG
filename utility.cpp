82b87034f5505aae3efa006928798b42a86a2705
DaniilAnd <lisichkindan@yandex.ru>
2019-03-22 02:12:59 +0300

    correct func

--- utility.cpp
+++ utility.cpp
@@ -1,166 +1,55 @@
-﻿#include <iostream>
-
-
-#include <iomanip>
-#include <istream>
-#include "utility.hpp"
-
-std::experimental::optional<std::pair<unsigned, unsigned> > AddToZigZagMatrix(
-          boost::numeric::ublas::matrix<uint16_t> matrix
-        , std::pair<unsigned, unsigned> position
-        , uint16_t value)
-{
-
-    return {};
-    //#define Up true
-    //#define Down false
-    //#define setQuantTableItem(bT,aT,ix,iy,iT) ({aT[iy][ix]=bT;iT++;})
-    //    typedef bool Direction;
-
-    //    static Direction fDirection=Down;
-    //    static int iT=0;
-    //    static int ix=0,iy=0;
-    //    static int state=0;
-    //    switch (state) {
-    //    case 0: setQuantTableItem(bT,aTable,ix,iy,iT);/* начало функции */
-    //        //return true;
-    //        while (iT<cnMatrixDimension-2)
-    //        {
-    //            if (fDirection==Down)
-    //            {
-    //                if (ix<cnMatrixSide-1)++ix;else iy++;
-    //                for (ix,iy;ix>=0&&iy<cnMatrixSide;ix--,iy++)
-    //                {
-
-    //                    state = 1;/* возвратиться к "case 1" */
-    //                    return false;
-    //                case 1: setQuantTableItem(bT,aTable,ix,iy,iT);/* продолжить выполнения после точки возврата */;/* возвратиться к "case 1" */
-    //                }
-    //                ix++;
-    //                iy--;
-    //                fDirection=Up;
-    //            }
-    //            else
-    //            {
-    //                if (iy<cnMatrixSide-1)++iy;else ++ix;
-    //                for (ix,iy;ix<cnMatrixSide&&iy>=0;ix++,iy--)
-    //                {
-    //                    state = 2;/* возвратиться к "case 1" */
-    //                    return false;
-    //                case 2: setQuantTableItem(bT,aTable,ix,iy,iT);
-    //                }
-    //                ix--;
-    //                iy++;
-    //                fDirection=Down;
-    //            }
-    //        }
-    //        ix++;
-    //        state=3;
-    //        return false;
-    //    case 3: setQuantTableItem(bT,aTable,ix,iy,iT);
-    //    }
-    //    fDirection=Down;
-    //    iT=0;
-    //    ix=0;
-    //    iy=0;
-    //    state=0;
-    //    return true;
-    //#undef Up
-    //#undef Down
-    //#undef setQuantTableItem
-}
-
-
-
-
-#if 0
-
+﻿//#include "pch.h"
+#include<iostream>
+#include<conio.h>
+#include<iomanip>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <array>
+using std::cout;
+using std::endl;
+using std::array;
 using namespace std;
 
-int main()
+
+
+static int x;
+static const int SIZE = 8;
+static boost::numeric::ublas::matrix<int> A(SIZE, SIZE);
+
+//const array<int, 65> num = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,
+//14,15,16,17,18,19,20,21,22,
+//23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,
+//46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64 };
+
+
+int   ArrZigzag(array<int,65>num)
 {
-	const int colum = 10; // количество столбцов массива
-	const int row = 10; // количество строк массива
-	int array[100][100];
-
-	int x, y,  // Координаты текущего элемента массива
-		value = 1; // значение, которым заполняется массив
-
-	// зполнение первой половины массива по диагонали, зигзагом, начиная
-	// слева и сверху, заканчивая  побочной диагональю
-	for (int diag = 0; diag < colum; diag++) // выполняем проход по диагоналям
+	int count = 0;
+	for (int i = 0; i < SIZE; i++)
 	{
-		if (diag % 2 == 0) // по четным диагоналям
-		{
-			x = 0; // х-координата первого лемента массива на диагонали - diag
-			y = diag; // у-координата элемента массива на диагонали - diag
-
-			while (y >= 0) // пока y-координата находится в верхней части диагонали
+		x = i;
+		if (i % 2)
+			for (int j = 0; j <= i; j++, x--)
 			{
-				array[x][y] = value; // записать значение в массив
-				value++;
-				x++;     // по горизонтали, смещаемся влево
-				y--;    // по вертикали, смещаемся вниз
+				A(x, j) = count++;
+                A(SIZE - 1 - x, SIZE - 1 - j) = (num[65] - count + 1);
 			}
-		}
-		else // по нечетным диагоналям
-		{
-			x = diag; // х-координата элемента массива на диагонали - diag
-			y = 0; // у-координата первого элемента массива на диагонали - diag
-
-			while (x >= 0) // пока x-координата находится в левой части диагонали
+		else
+			for (int j = 0; j <= i; j++, x--)
 			{
-				array[x][y] = value; // записать значение в массив
-				value++;
-				x -= 1;  // по горизонтали, смещаемся вправо
-				y += 1; // по вертикали, смещаемся вверх
+				A(j, x) = count++;
+                A(SIZE - 1 - j, SIZE - 1 - x) = (num[65] - count + 1);
 			}
-		}
-	} // конец for
-
-	// заполнение второй половины массива по диагонали, зигзагом, начиная
-	// слева и сверху, заканчивая  последним элементом массива
-	for (int diag = 1; diag < colum; diag++)
-	{
-		if (diag % 2 == 0) // по четным диагоналям
-		{
-			x = 9; // х-координата первого элемента массива на диагонали - diag
-			y = diag;  // у-координата элемента массива на диагонали - diag
-
-			while (y <= 9) // Пока не кончилась диагональ
-			{
-				array[x][y] = value;
-				value++;
-				x--; // по горизонтали, смещаемся влево
-				y++; // по вертикали, смещаемся вниз
-			}
-		}
-		else // по не четным диагоналям
-		{
-			x = diag; // х-координата первого элемента к-ой диагонали
-			y = 9;  // у-координата первого элемента к-ой диагонали
-
-			while (x <= 9) // Пока не кончилась диагональ
-			{
-				array[x][y] = value;
-				value++;
-				x++; // по горизонтали, смещаемся вправо
-				y--; // по вертикали, смещаемся вверх
-			}
-		} // конец if-else
-	} // конец цикла for (заполнение второй половины массива)
-
-	// вывод масиива на экран
-	for (int ix = 0; ix < row; ix++)
-	{
-		for (int jx = 0; jx < colum; jx++)
-            cout  << array[ix][jx] << " " << endl;
-		/*cout  << array[ix][jx] << " " << endl;*/
-
-
 	}
-
-	return 0;
+// вывод 
+	for (int i = 0; i < SIZE; i++)
+	{
+		for (int j = 0; j < SIZE; j++)
+			cout << setw(4) << A(i, j);
+		cout << endl;
+	}
+    return 0;
 }
 
-#endif
+
+
+
