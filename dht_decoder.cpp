33b5f6f00c58aeca54234617198b540ca22d9654
acsvln <acsvln@disroot.org>
2020-01-25 01:39:25 +0300

    Added DHTDecoder module

---
+++ dht_decoder.cpp
@@ -0,0 +1,6 @@
+#include "dhtdecoder.h"
+
+DHTDecoder::DHTDecoder()
+{
+
+}
---
+++ dht_decoder.h
@@ -0,0 +1,11 @@
+#ifndef DHTDECODER_H
+#define DHTDECODER_H
+
+
+class DHTDecoder : public IDecoder
+{
+public:
+    DHTDecoder();
+};
+
+#endif // DHTDECODER_H
