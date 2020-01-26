1b1444e9b70c17b1b84b8ef31efe913f13e8c45d
acsvln <acsvln@disroot.org>
2020-01-27 02:20:46 +0300

    Added CreateZigZagMatrix function and implemented tests for it

--- utility.hpp
+++ utility.hpp
@@ -3,8 +3,10 @@
 
 #include <cstddef>
 #include <string>
+#include <array>
 
 #include <boost/endian/conversion.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
 
 template<class NumberType, class StreamType>
 NumberType ReadNumberFromStream( StreamType& aStream )
@@ -17,5 +19,7 @@ NumberType ReadNumberFromStream( StreamType& aStream )
 }
 
 void printSectionDescription(const std::string& name, std::size_t size);
+
+boost::numeric::ublas::matrix<uint16_t> CreateZigZagMatrix( std::array<uint16_t, 64> source );
 
 #endif // UTILITY_HPP
