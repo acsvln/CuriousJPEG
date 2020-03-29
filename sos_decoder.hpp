17db0d60552917cb2e3110c2aaf84724b405affe
tayursky_k <tkostia.a@yandex.ru>
2020-03-29 15:43:54 +0300

    Implemented draft of SOSDecoder

--- sos_decoder.hpp
+++ sos_decoder.hpp
@@ -3,8 +3,30 @@
 
 #include "decoder.hpp"
 
+#include <bitset>
+
 class SOSDecoder : public IDecoder
 {
+public:
+    class BitExtractor final {
+    public:
+        explicit
+        BitExtractor( std::istream &aStream );
+        uint8_t nextNumber();
+        uint8_t readBits( std::size_t cnt );
+
+    private:
+        std::bitset<8> mbits;
+        std::size_t mCounter = 0;
+        std::istream &mStream;
+    };
+
+    static
+    boost::numeric::ublas::matrix<uint8_t> ReadMatrix(
+        BitExtractor&,
+        const std::shared_ptr<DHTNode>& AC_Table,
+        const std::shared_ptr<DHTNode>& DC_Table );
+
 public:
     ~SOSDecoder() final = default;
 
