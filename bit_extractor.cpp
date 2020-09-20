9d56a6ec6b859c1499cb97ec56216245d3ef86ae
acsvln <acsvln@disroot.org>
2020-09-20 23:23:37 +0300

    Added temporary nextNumber16 function to BitExtractor

--- bit_extractor.cpp
+++ bit_extractor.cpp
@@ -1,6 +1,11 @@
 #include "bit_extractor.hpp"
 
+#include "data_reader.hpp"
+
 #include <boost/assert.hpp>
+
+#include <iostream>
+#include <bitset>
 
 BitExtractor::BitExtractor( std::istream &Strm )
     : Stream{ Strm }
@@ -15,10 +20,37 @@ auto BitExtractor::nextNumber(std::size_t const BitCnt) -> uint8_t {
   for ( std::size_t Index = BitCnt; Index != 0 ; --Index ) {
       if (0 == Counter) {
         Bits = static_cast<unsigned>(Stream.get());
+
         Counter = Bits.size();
       }
       Counter--;
       Result[Index - 1] = Bits[Counter];
   }
   return static_cast<uint8_t>(Result.to_ulong());
+}
+
+auto BitExtractor::nextNumber16(std::size_t const BitCnt) -> uint16_t {
+    BOOST_ASSERT_MSG(BitCnt > 0 && BitCnt <= 16,
+                     "Bit count must be between 0 and 16");
+
+    std::bitset<16> Result;
+
+    for ( std::size_t Index = BitCnt; Index != 0 ; --Index ) {
+        if (0 == Counter16) {
+            //
+        // TODO: Bitset
+//          uint16_t t;
+//          DataReader::readSruct( Stream, t );
+//          Bits16 = t;
+            Bits16 = DataReader::readNumber<uint16_t>(Stream);
+
+
+          Counter16 = Bits16.size();
+        }
+
+        Counter16--;
+        Result[Index - 1] = Bits16[Counter16];
+    }
+
+    return static_cast<uint16_t>(Result.to_ulong());
 }
--- bit_extractor.hpp
+++ bit_extractor.hpp
@@ -1,18 +1,21 @@
 #ifndef BITEXTRACTOR_HPP
 #define BITEXTRACTOR_HPP
 
-#include <bitset>
 #include <istream>
+#include <bitset>
 
 class BitExtractor final {
 public:
   explicit BitExtractor(std::istream &Stream);
 
   auto nextNumber(std::size_t const BitCnt = 1) -> uint8_t;
+  auto nextNumber16(std::size_t const BitCnt = 1) -> uint16_t;
 
 private:
   std::bitset<8> Bits;
   std::size_t Counter = 0;
+  std::bitset<16> Bits16;
+  std::size_t Counter16 = 0;
   std::istream &Stream;
 };
 
